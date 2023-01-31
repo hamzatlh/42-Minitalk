3047621f3e79b6c1766b9c49f2ad32738584ce65
hamzatlh <htlhaouia@gmail.com>
2023-01-31 20:19:01 +0100

    minitalk

---
+++ client.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htalhaou <htalhaou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/28 01:22:43 by htalhaou          #+#    #+#             */
+/*   Updated: 2023/01/31 20:14:36 by htalhaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+#include<stdlib.h>
+#include<signal.h>
+#include<stdio.h>
+
+int	ft_atoi(const char *str)
+{
+	int	res;
+	int	sign;
+	int	i;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
+
+void	send_character(char c, int pid)
+{
+	int	i;
+	int	bit;
+	int	tmp;
+
+	i = 7;
+	while (i >= 0)
+	{
+		bit = ((c >> i) & 1);
+		if (bit == 0)
+			tmp = kill(pid, SIGUSR1);
+		else
+			tmp = kill(pid, SIGUSR2);
+		if (tmp == -1)
+			exit (1);
+		usleep (200);
+		i--;
+	}
+}
+
+void	send_string(char *str, int pid)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		send_character(str[i], pid);
+		i++;
+	}
+	send_character('\0', pid);
+}
+
+void	hand_msg(int sig)
+{
+	write(1, "message recieved\n", 17);
+}
+
+int	main(int ac, char **av)
+{
+	int	pid;
+
+	signal(SIGUSR2, &hand_msg);
+	if (ac != 3)
+		return (0);
+	pid = ft_atoi(av[1]);
+	if (pid <= 1)
+		return (0);
+	send_string(av[2], pid);
+	return (0);
+}
---
+++ server.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htalhaou <htalhaou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/28 01:22:39 by htalhaou          #+#    #+#             */
+/*   Updated: 2023/01/31 20:05:35 by htalhaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<signal.h>
+#include<unistd.h>
+#include<stdio.h>
+
+void	sig_handler(int signum, siginfo_t *info, void *upp)
+{
+	static char		c;
+	static int		countbit;
+	static int		pid;
+
+	(void)upp;
+	if (pid != info->si_pid)
+	{
+		countbit = 0;
+		c = 0;
+		pid = info->si_pid;
+	}
+	c <<= 1;
+	if (signum == SIGUSR2)
+		c = c | 1;
+	countbit++;
+	if (countbit == 8)
+	{
+		write(1, &c, 1);
+		countbit = 0;
+		if (c == '\0')
+			kill(pid, SIGUSR2);
+	}
+}
+
+int	main(void)
+{
+	pid_t				pid;
+	struct sigaction	sa;
+
+	pid = getpid();
+	printf("PID : %d\n", pid);
+	while (1)
+	{
+		sa.sa_sigaction = sig_handler;
+		sigaction(SIGUSR1, &sa, NULL);
+		sigaction(SIGUSR2, &sa, NULL);
+		pause();
+	}
+}
